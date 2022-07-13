c1525dcf58a94cc43740330a07e93c8b332693f8
zkan <zkan.cs@gmail.com>
2022-07-13 12:36:25 +0700

    Load files from S3 and merge them in one single file

--- mnt/dags/load_networkrail_movement_data_to_bigquery.py
+++ mnt/dags/load_networkrail_movement_data_to_bigquery.py
@@ -1,6 +1,36 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3_list import S3ListOperator
 from airflow.utils import timezone
+
+
+S3_BUCKET = "networkrail"
+
+
+def _download_files(**context):
+    ds = context["ds"]
+    # year, month, day = ds.split("-")
+
+    s3_hook = S3Hook(aws_conn_id="minio")
+
+    events = []
+    files = context["ti"].xcom_pull(task_ids="list_files", key="return_value")
+    for each in files:
+        file_name = s3_hook.download_file(
+            key=each,
+            bucket_name=S3_BUCKET,
+        )
+
+        with open(file_name) as f:
+            events.append(f.read())
+
+    output_file_name = f"{ds}-events.json"
+    with open(output_file_name, "w") as f:
+        f.writelines(f"{e}\n" for e in events)
+
+    return output_file_name
 
 
 default_args = {
@@ -16,6 +46,18 @@ with DAG(
 
     start = EmptyOperator(task_id="start")
 
+    list_files = S3ListOperator(
+        task_id="list_files",
+        bucket=S3_BUCKET,
+        prefix='year={{ macros.ds_format(logical_date | ds, "%Y-%m-%d", "%Y")  }}/month={{ macros.ds_format(logical_date | ds, "%Y-%m-%d", "%m") }}/day={{ macros.ds_format(logical_date | ds, "%Y-%m-%d", "%d") }}/',
+        aws_conn_id="minio",
+    )
+
+    download_files = PythonOperator(
+        task_id="download_files",
+        python_callable=_download_files,
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> end
+    start >> list_files >> download_files >> end

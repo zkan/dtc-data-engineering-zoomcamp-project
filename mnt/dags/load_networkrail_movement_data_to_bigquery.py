40be7be56c3a7913194b74ec4179ffa59a440758
zkan <zkan.cs@gmail.com>
2022-07-13 14:34:09 +0700

    Load data to BigQuery

--- mnt/dags/load_networkrail_movement_data_to_bigquery.py
+++ mnt/dags/load_networkrail_movement_data_to_bigquery.py
@@ -1,22 +1,36 @@
+import logging
+from datetime import datetime
+from pytz import timezone as tz
+
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.s3_list import S3ListOperator
 from airflow.utils import timezone
 
+import google
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
+
+TIMEZONE_LONDON: tz = tz("Europe/London")
+S3_CONN = "minio"
 S3_BUCKET = "networkrail"
+VARIABLE_BIGQUERY_CREDENTIAL_SECRET = "bigquery_credential_secret"
+VARIABLE_BIGQUERY_PROJECT_ID = "bigquery_project_id"
+BIGQUERY_TABLE_ID = "networkrail.movements"
 
 
-def _download_files(**context):
+def _download_files(**context) -> str:
     ds = context["ds"]
-    # year, month, day = ds.split("-")
+    files = context["ti"].xcom_pull(task_ids="list_files", key="return_value")
 
-    s3_hook = S3Hook(aws_conn_id="minio")
+    s3_hook = S3Hook(aws_conn_id=S3_CONN)
 
     events = []
-    files = context["ti"].xcom_pull(task_ids="list_files", key="return_value")
     for each in files:
         file_name = s3_hook.download_file(
             key=each,
@@ -31,6 +45,73 @@ def _download_files(**context):
         f.writelines(f"{e}\n" for e in events)
 
     return output_file_name
+
+
+def remove_old_data(bq_client: bigquery.Client, table: str, ds: str) -> None:
+    try:
+        query = bq_client.query(
+            f"""
+            DELETE FROM `{table}` WHERE DATE(actual_timestamp) = "{ds}"
+            """
+        )
+        query.result()
+    except google.api_core.exceptions.NotFound:
+        logging.info("No table found")
+
+
+def _load_data_to_bigquery(**context) -> None:
+    ds = context["ds"]
+    file_name = context["ti"].xcom_pull(task_ids="download_files", key="return_value")
+    df = pd.read_json(file_name, dtype=str, lines=True)
+
+    df["actual_timestamp"] = df["actual_timestamp"].astype(int) / 1000
+    df["actual_timestamp"] = df["actual_timestamp"] \
+        .map(datetime.utcfromtimestamp) \
+        .map(TIMEZONE_LONDON.fromutc)
+
+    bigquery_schema = [
+        bigquery.SchemaField("event_type", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("gbtt_timestamp", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("original_loc_stanox", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("planned_timestamp", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("timetable_variation", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("original_loc_timestamp", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("current_train_id", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("delay_monitoring_point", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("next_report_run_time", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("reporting_stanox", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("actual_timestamp", bigquery.enums.SqlTypeNames.TIMESTAMP),
+        bigquery.SchemaField("correction_ind", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("event_source", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("train_file_address", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("platform", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("division_code", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("train_terminated", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("train_id", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("offroute_ind", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("variation_status", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("train_service_code", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("toc_id", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("loc_stanox", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("auto_expected", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("direction_ind", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("route", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("planned_event_type", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("next_report_stanox", bigquery.enums.SqlTypeNames.STRING),
+        bigquery.SchemaField("line_ind", bigquery.enums.SqlTypeNames.STRING),
+    ]
+    job_config = bigquery.LoadJobConfig(schema=bigquery_schema)
+
+    bq_credential_secret = Variable.get(VARIABLE_BIGQUERY_CREDENTIAL_SECRET, deserialize_json=True)
+    bq_client = bigquery.Client(
+        credentials=service_account.Credentials.from_service_account_info(bq_credential_secret),
+        project=Variable.get(VARIABLE_BIGQUERY_PROJECT_ID),
+    )
+
+    remove_old_data(bq_client, BIGQUERY_TABLE_ID, ds)
+
+    job = bq_client.load_table_from_dataframe(df, BIGQUERY_TABLE_ID, job_config=job_config)
+    job.result()
 
 
 default_args = {
@@ -58,6 +139,11 @@ with DAG(
         python_callable=_download_files,
     )
 
+    load_data_to_bigquery = PythonOperator(
+        task_id="load_data_to_bigquery",
+        python_callable=_load_data_to_bigquery,
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> list_files >> download_files >> end
+    start >> list_files >> download_files >> load_data_to_bigquery >> end
